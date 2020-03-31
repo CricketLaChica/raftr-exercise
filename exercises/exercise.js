bca4491045149db2f3bd0b9e628bc12d107f4e79
Cricket La Chica <cricketlachica@gmail.com>
2020-03-31 09:13:42 -1000

    Added exercises.

---
+++ exercises/exercise.js
@@ -0,0 +1,25 @@
+/*
+ * EXERCISE
+ */
+
+const list1 = ["a", "b", "c", "moreThan5"]
+// expected result for list 1:
+// { a: false, b: false, c: false, moreThan5: true }
+
+const list2 = ["abcd", "bcdefg", "c", "amoreThan6"]
+// expected result for list 2:
+// { abcd: false, bcdefg: true, c: false, amoreThan6: true }
+
+/**
+ * complete this function that receives a list of strings
+ * and it returns a list of key: values
+ * where the key is the current item of the list
+ * and the value is true|false depending if
+ * the string has more|less than 5 characters
+ */
+function process(list) {
+  // Process the list, for example:
+  // reduce(_ => {
+  //   return null;
+  // }, {});
+}
